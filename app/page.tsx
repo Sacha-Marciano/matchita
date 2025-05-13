aaa3a385b5bd1d73f96a929fb24bf183c77fac84
Sacha-Marciano <Momarciano26@gmail.com>
2025-05-13 09:48:52 +0300

    test - optimized build

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import RoomModal from "./components/RoomModal";
 
 interface Room {
